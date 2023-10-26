d996be6787a4d941f7405e545915cbc53227a5d0
Henry-Best-01 <51093468+Henry-Best-01@users.noreply.github.com>
2023-10-26 12:32:18 -0400

    Update variability.py
    Added movie interpolator function
    
    Will work on test sets soon

--- slsim/Sources/source_variability/variability.py
+++ slsim/Sources/source_variability/variability.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 """This class aims to have realistic variability models for AGN and Supernovae."""
 
@@ -42,3 +43,77 @@ def sinusoidal_variability(t, amp, freq):
     :return: variability for the given time
     """
     return amp * np.sin(2 * np.pi * freq * t)
+
+
+def interpolate_movie(Movie, Orig_timestamps, New_timestamps, verbose=False, plot=False):
+    '''
+    This function aims to take a series of snapshots and resample them at different time intervals.
+
+    Movie is expected to be an array of (time, space_x, space_y),
+    and Orig_timestamps is an array of timestamps representing the time values of each snapshot.
+    New_timestamps is a list or array of new time stamps using the same units as Orig_timestamps.
+
+    verbose allows some information to be printed to standard output.
+    plot creates two plots, one with the original light curve plotted with the new interpolated light curve,
+    and one where the second frame of each image is plotted side by side.
+
+    returns an array representing the new resampled movie at timestamps New_timestamps, of shape
+    (len(New_timestamps, np.size(Movie, 1), np.size(Movie, 2))
+    '''
+
+    initial_shape = np.shape(Movie)
+    if verbose: print("The initial movie shape was",initial_shape)
+    npix = initial_shape[1] * initial_shape[2]
+    if verbose: print("This had",npix,"pixels")
+    if verbose: print("Data will be interpolated to",len(New_timestamps), "frames")
+
+    space_positions = np.linspace(1, npix, npix)                        # Define linear positions of pixels
+
+    intermediate_movie = np.reshape(Movie, (initial_shape[0], npix))    # reshape image to a line
+
+    interpolation = scipy.interpolate.RegularGridInterpolator((Orig_timestamps, space_positions),
+                                            intermediate_movie, bounds_error=False, fill_value=None)
+
+    new_timelength = np.size(New_timestamps)
+    new_points_t, new_points_s = np.meshgrid(New_timestamps, space_positions, indexing='ij')
+    movie_resampled = interpolation((new_points_t, new_points_s))
+    if verbose: print("The new interpolated movie is now shape",np.shape(movie_resampled),"in time and spatial coordinates")
+
+    # unpack to movie shape (time axis is and was first)
+    resampled_movie = np.reshape(movie_resampled, (new_timelength, initial_shape[1], initial_shape[2])) 
+    if verbose: print("The reconstructed movie is now shape",np.shape(resampled_movie))
+
+    if plot:
+        import matplotlib.pyplot as plt
+        fig, ax = plt.subplots()
+
+        ax.plot(np.asarray(Orig_timestamps), np.sum(Movie, axis=(1,2)), linewidth=2)
+        ax.plot(np.asarray(New_timestamps), np.sum(resampled_movie, axis=(1,2)), '-o',alpha=0.5)
+
+        ax.set_xlabel("Time [days]")
+        ax.set_ylabel("Flux [arb]")
+        
+        fig2, ax2 = plt.subplots(1,2)
+
+        conts1 = ax2[0].contourf(Movie[1])
+        conts2 = ax2[1].contourf(resampled_movie[1])
+
+        ax2[0].set_xlabel("x [px]")
+        ax2[1].set_xlabel("x [px]")
+        ax2[0].set_ylabel("y [px]")
+        ax2[1].set_ylabel("y [px]")
+        ax2[0].set_title("Orig. movie, frame 2")
+        ax2[1].set_title("Interp. movie, frame 2")
+
+        plt.colorbar(conts1, ax=ax2[0], label="Flux [arb.]")
+        plt.colorbar(conts2, ax=ax2[1], label="Flux [arb.]")
+
+
+        for axis in ax2:
+            axis.set_xlim(4*initial_shape[1]/9, 5*initial_shape[1]/9)
+            axis.set_ylim(4*initial_shape[2]/9, 5*initial_shape[2]/9)
+            axis.set_aspect(1)
+        plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1, wspace=0.3)
+        
+        plt.show()
+    return resampled_movie   
